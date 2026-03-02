07b14b894fa0b18bdc311120f00ae21d561417f5
botelhool <leo.obotelho@outlook.com>
2026-03-02 12:00:17 -0300

    ✨ New Feature: Construção do recurso Postagem

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,24 @@
+import { Transform, TransformFnParams } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name:"tb_postagens"})//Create table tb_postagens
+export class Postagem{
+    
+    @PrimaryGeneratedColumn() // PRIMARY KEY (id) AUTO INCREMENT
+    id:number;
+    
+    @Transform(({value}:TransformFnParams)=> value?.trim())
+    @IsNotEmpty() // validador
+    @Column({length:100,nullable:false})// Varchar(100) NOT NULL
+    título: string;
+    
+    @Transform(({value}:TransformFnParams)=> value?.trim())
+    @IsNotEmpty() // validador
+    @Column({length:1000,nullable:false})// Varchar(1000) NOT NULL
+    texto:string;
+    
+    @UpdateDateColumn()
+    data:Date;
+
+}
