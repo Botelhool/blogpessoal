e38e5a89f8f8aeb05d685e67bebe7523eb761c0f
botelhool <leo.obotelho@outlook.com>
2026-03-16 19:43:16 -0300

    ✨ New Feature: Adiciona suporte ao ambiente de desenvolvimento e produção com configuração do TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/services/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 
 @Module({
   imports: [
-   TypeOrmModule.forRoot({
-  type: 'postgres', // Neon usa PostgreSQL
-  url: process.env.DATABASE_URL, // A URL de conexão é fornecida pelo Neon
-  entities: [Postagem, Tema, Usuario],
-  synchronize: true, // Em produção, prefira usar migrations
-  ssl: true, // Ativa o SSL (obrigatório no Neon)
-  extra: {
-    ssl: {
-      rejectUnauthorized: false, // Necessário para aceitar o certificado do Neon no Render
-    },
-  },
-}),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
     PostagemModule,
     TemaModule,
     AuthModule,
@@ -33,4 +25,4 @@ import { AppController } from './app.controller';
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
